dd0467a949c9c070d9310abe4a7807aa533f973e
Mikael Larsson <mikael@fruktleveransen.se>
2015-01-25 19:37:33 +0100

    Registreringsformulär

--- subscriber.php
+++ subscriber.php
@@ -110,4 +110,182 @@ function myajax_subscriber_submit_func(){
 	}
 }
 
+function display_form() {
+
+	add_thickbox();
+
+	?>
+
+	<script>
+		function createCookie(name,value,days) {
+			if (days) {
+				var date = new Date();
+				date.setTime(date.getTime()+(days*24*60*60*1000));
+				var expires = "; expires="+date.toGMTString();
+			} else { 
+				var expires = "";
+				document.cookie = name+"="+value+expires+"; path=/";
+			}
+		}
+
+		function readCookie(name) {
+			var nameEQ = name + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0;i < ca.length;i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1,c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			}
+			return null;
+		}
+
+		jQuery(document).ready(function() {
+
+			// Check cookie
+			if(readCookie('SubsriberForm-AshleyCooper') || readCookie('AshleyCooper-VisitorSubsribed')) {
+				var displayForm = false;
+			} else {
+				var displayForm = true;
+				createCookie('SubsriberForm-AshleyCooper', 'session');
+			}
+
+			// Popup timer
+			if( displayForm ){
+				setTimeout(function() {
+					var t 	= 'Subscribe to my newsletter';
+					var a 	= "#TB_inline?width=625&amp;height=250&amp;inlineId=newsletter-thickbox";
+					var g 	= false;
+					tb_show(t, a, g);
+				}, 1000);
+			}
+
+			// Handle form submission
+			jQuery('form#mailchimp-subscribe').submit(function( event ){
+
+				// Prevent submision
+				event.preventDefault();
+
+				// Disable submit button
+				jQuery('#mailchimp-submit-button').disable();
+
+				// Collect vars
+				var form 	= jQuery(this);
+				var action 	= form.attr('action');
+				var name 	= jQuery('input[name=name]', form).val();
+				var email 	= jQuery('input[name=email]', form).val();
+				var formData = {
+					'action': 	'myajax_subscriber_submit_func',
+					'name': 	name,
+					'email': 	email
+				};
+
+				// Validate email
+				var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				if( !regex.test( email ) ){
+					jQuery('#newsletter-thickbox-content .alert p').text('Please enter a valid email address.');
+					jQuery('#newsletter-thickbox-content .alert').show().delay(3000).fadeOut(2000);
+					return false;
+				}
+
+				// Make Ajax call
+				jQuery.post( action, formData, function( data ){
+					if( data.status == 'error' ){
+
+						// Enable submit button
+						jQuery('#mailchimp-submit-button').enable();
+						
+						// Show error message
+						jQuery('#newsletter-thickbox-content .form').hide();
+						jQuery('#newsletter-thickbox-content .error').show();
+					} else {
+
+						// Thank you message
+						jQuery('#newsletter-thickbox-content .form').hide();
+						jQuery('#newsletter-thickbox-content .thank-you .email-placeholder').text( email );
+						jQuery('#newsletter-thickbox-content .thank-you').show();
+
+						// Set permanent cookie
+						createCookie('AshleyCooper-VisitorSubsribed', 'set', 3650);
+					}
+				}, "json");
+			});
+		});
+	</script>
+	
+	<div id="newsletter-thickbox">
+		<style>
+			#newsletter-thickbox-content .left {
+				float: 		left;
+			}
+			#newsletter-thickbox-content .text-input-container {
+				width: 		238px;
+				padding-right: 20px;
+			}
+			#newsletter-thickbox-content .text-input-container input {
+				border:		1px solid #D2D2D2;
+				padding: 	8px 2%;
+				width: 		100%;
+			}
+			#TB_ajaxWindowTitle {
+				visibility: none;
+				display: none;
+			}
+			#newsletter-thickbox-content .alert {
+				border: 			1px solid #99B7DB;
+				background-color: 	#C1E0FF;
+				padding: 			10px;
+			}
+			#newsletter-thickbox-content .alert p {
+				margin-top: 0;
+			}
+			#newsletter-thickbox-content .thank-you {
+				text-align: center;
+			}
+		</style>
+	     <div id="newsletter-thickbox-content">
+
+	     	<!-- Form -->
+	     	<div class="form">
+		     	<h1>Subscribe to my monthly newsletter!</h1>
+
+		     	<!-- Alert box -->
+		     	<div class="alert" style="display: none;"><p></p></div>
+
+		     	<form action="/wp-admin/admin-ajax.php" method="post" id="mailchimp-subscribe">
+					<input type="hidden" name="register">
+
+					<!-- Name -->
+					<div class="left text-input-container">
+						<label for="name">Name </label><br>
+						<input type="text" name="name" placeholder="Your name" class="text-input">
+					</div>
+
+					<!-- Email -->
+					<div class="left text-input-container">
+						<label for="email">Email</label><br>
+						<input type="email" name="email" placeholder="Your email" class="text-input">
+					</div>
+					<input type="submit" value="Send" id="mailchimp-submit-button" class="left button large button darkgray fusion-button button-flat button-square button-large button-darkgray button-2 buttonshadow-0">
+				</form>
+			</div>
+
+		     <!-- Thank you note -->
+			<div class="thank-you" style="display: none;">
+				<h1>Thank you!</h1>
+				<p><span class="email-placeholder">[Email]</span> successfuly added to subsriber list.</p>
+			</div>
+
+		     <!-- Error note -->
+			<div class="error" style="display: none;">
+				<h1>Oops!</h1>
+				<p>There was a technical error. Try again later.</p>
+			</div>
+	     </div>
+	</div>
+
+	<?php
+}
+
+add_action('wp_footer', 'display_form');
+
 ?>
